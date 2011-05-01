efe470022730f27875b837c18e8d847c4ea9ec47
Justin T. Conroy <jtconroy88@gmail.com>
2011-04-30 20:49:18 -0500

    A little refactoring...

--- gamescores/models.py
+++ gamescores/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from decimal import *
+from datetime import *
 
 # Creates table listing all players for all games
 class Player(models.Model):
@@ -15,53 +17,79 @@ class Game(models.Model):
 		return self.name
 
 # Abstract class for recording the result of each game played
-class Common_Game(models.Model):
+class CommonGame(models.Model):
 	game = models.ForeignKey(Game)
 	date_played = models.DateField(default=datetime.today())
 
 	def __unicode__(self):
-		return self.date_played
+		return str(self.date_played)
 
 	class Meta:
 		abstract = True
 
 # Abstract model for the players to be listed in each played game.
-class Game_Player(models.Model):
+class GamePlayer(models.Model):
 	player = models.ForeignKey(Player)
 	score = models.IntegerField(default=0)
 
 	def __unicode__(self):
-		return self.player
+		return self.player.name
 
 	class Meta:
 		abstract = True
 
 # Abstract model for statistics on each game that a player has played.
-class Game_Player_Stats(models.Model):
+class GamePlayerStats(models.Model):
 	game = models.ForeignKey(Game)
-	player = moedels.ForeignKey(Player)
+	player = models.ForeignKey(Player)
 	total_games_played = models.IntegerField(default=0,editable=False)
 	total_victory_points = models.IntegerField(default=0,editable=False)
-	average_victory_points = models.DecimalField(max_digits=5, decimal_places=default=0,editable=False)
+#	average_victory_points = models.DecimalField(max_digits=5, decimal_places=default=0,editable=False)
 	rating = models.IntegerField(default=1000,editable=False)
 	wins = models.DecimalField(max_digits=5, decimal_places=3, default=0, editable=False)
 	losses = models.DecimalField(max_digits=5, decimal_places=3, default=0, editable=False)
 
 	def __unicode__(self):
-		return self.player
+		return self.player.name
+
+	# Returns average number of victory points per game
+	def _get_average_victory_points(self):
+		if (self.total_games_played > 0):
+			return Decimal(self.total_victory_points)/self.total_games_played
+		else:
+			return 0
+
+	# Returns ratio of wins to losses
+	def _get_win_loss_ratio(self):
+		if (self.losses > 0):
+			return self.wins/self.losses
+		else:
+			if (self.wins > 0):
+				return 999999999999999.99
+			else:
+				return 0
+
+	average_victory_points = property(_get_average_victory_points)
+	win_loss_ratio = property(_get_win_loss_ratio)
+	
+	# Calculate a new rating
+#	def calculate_rating(self, 
+
 
 	class Meta:
 		abstract = True
 
 # fields specific to Settlers of Catan
-class Catan_Game(Common_Game):
-	game.default = game_set.filter(name='Settlers of Catan')
-	game.editable = False
+class CatanGame(CommonGame):
+#	game.default = Common_Game.gamefilter(name='Settlers of Catan')
+	CommonGame.game.default = 1
+	CommonGame.game.editable = False
 
-class Catan_Player(models.Model):
+class CatanPlayer(GamePlayer):
+	game = models.ForeignKey(CatanGame)
 
-
-class Catan_Player_Stats(Game_Player_Stats):
-	game.default = game_set.filter(name='Settlers of Catan')
-	game.editable = False
+class CatanPlayerStats(GamePlayerStats):
+#	game.default = game_set.filter(name='Settlers of Catan')
+	CommonGame.game.default = 1
+	CommonGame.game.editable = False
 
